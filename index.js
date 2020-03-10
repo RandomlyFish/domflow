1118f06417afd981d01ee5798cea005bbbd221e8
RandomlyFish <Tobias.Rheindorf@gmail.com>
2020-03-10 22:15:41 +0100

    Fixed code completion for created elements

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+// This is needed for code completion for created elements
+require("./src/DomButton");
+require("./src/DomContainer");
+require("./src/DomElement");
+require("./src/DomFlexContainer");
+require("./src/DomIcon");
+require("./src/DomLayer");
+require("./src/DomText");
+require("./src/DomToggle");
+
 export {default as dom} from "./src/dom";
 export {default as IoniconsV4} from "./src/icon_fonts/ioniconsV4";
